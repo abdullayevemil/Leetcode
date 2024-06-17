3f205adf60ddf33fe5f2759fa8c198e8b448c2d0
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-06-18 00:55:03 +0400

    Add Palindrom number solution

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,31 @@
+﻿var solution = new Solution();
+
+var number = 121;
+
+var isPalindrome = solution.IsPalindrome(number);
+
+Console.WriteLine(isPalindrome);
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        int reversedNumber = 0;
+
+        int copy = x;
+
+        while (copy != 0)
+        {
+            reversedNumber = reversedNumber * 10 + copy % 10;
+
+            copy /= 10;
+        }
+
+        return x == reversedNumber;
+    }
+}
