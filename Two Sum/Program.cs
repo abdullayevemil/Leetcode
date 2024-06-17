7f9ad7d4dfc5312464a3b758f5a6246798db4c58
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-06-18 02:05:52 +0400

    Add Two sum solution

---
+++ Two Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿var solution = new Solution();
+
+var numbers = new int[] { 3, 3 };
+
+var target = 6;
+
+var indexes = solution.TwoSum(numbers, target);
+
+foreach (var index in indexes)
+{
+    System.Console.WriteLine(index);
+}
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[] { };
+    }
+}
