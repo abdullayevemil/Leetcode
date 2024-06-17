a4ad777d4f47a44d59fc995bb0b3211ff627b5f7
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-06-18 01:42:25 +0400

    Add Index of the First Occurrence in a String solution

---
+++ Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,21 @@
+﻿var solution = new Solution();
+
+var index = solution.StrStr("sadbutsad", "sad");
+
+Console.WriteLine(index);
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if (needle == haystack.Substring(i, needle.Length))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
