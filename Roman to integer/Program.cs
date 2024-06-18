2c20e4c6f943c1e9e354c44535213a8410ca6973
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-06-18 15:13:57 +0400

    Add Roman to integer solution

---
+++ Roman to integer/Program.cs
@@ -0,0 +1,55 @@
+﻿var solution = new Solution();
+
+var roman = "LVIII";
+
+var integer = solution.RomanToInt(roman);
+
+System.Console.WriteLine(integer);
+
+public class Solution
+{
+    public int GetValue(char digit)
+    {
+        if (digit == 'I') return 1;
+
+        if (digit == 'V') return 5;
+
+        if (digit == 'X') return 10;
+
+        if (digit == 'L') return 50;
+
+        if (digit == 'C') return 100;
+
+        if (digit == 'D') return 500;
+
+        if (digit == 'M') return 1000;
+
+        return -1;
+    }
+
+    public int RomanToInt(string s)
+    {
+        var number = 0;
+
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            var value = GetValue(s[i]);
+
+            var nextValue = GetValue(s[i + 1]);
+
+            if (value < nextValue)
+            {
+                number -= value;
+            }
+            else
+            {
+
+                number += value;
+            }
+        }
+
+        number += GetValue(s.Last());
+
+        return number;
+    }
+}
